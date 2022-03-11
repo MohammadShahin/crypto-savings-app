d8d770e3532f951a03a14e9a89a26dbd127f76d6
hadi <hadi.mohammad.saleh1@gmail.com>
2022-03-11 14:54:44 +0300

    added barebone Solana contract and test

--- savus-solana/programs/savus-solana/src/lib.rs
+++ savus-solana/programs/savus-solana/src/lib.rs
@@ -1,14 +1,68 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("G2GC7QFB5zaoyjmjNVRXoPZeCBG9YzD4BFNuAFmVBUqw");
 
 #[program]
 pub mod savus_solana {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn create_user_stats(
+        ctx: Context<CreateUserStats>
+    ) -> Result<()> {
+        let user_stats = &mut ctx.accounts.user_stats;
+        user_stats.expiration = 0;
+        user_stats.goal_amount = 0;
+        user_stats.bump = *ctx.bumps.get("user_stats").unwrap();
+        Ok(())
+    }
+
+    pub fn set_expiration(ctx: Context<SetExpiration>, expiration: u128) -> Result<()> {
+        if ctx.accounts.user_stats.expiration != 0 {
+            panic!();
+        }
+        ctx.accounts.user_stats.expiration = expiration;
+        Ok(())
+    }
+
+    pub fn set_goal_amount(ctx: Context<SetGoalAmount>, goal_amount: u128) -> Result<()> {
+        if ctx.accounts.user_stats.goal_amount != 0 {
+            panic!();
+        }
+
+        ctx.accounts.user_stats.goal_amount = goal_amount;
         Ok(())
     }
 }
 
+#[account]
+pub struct UserStats {
+    expiration: u128,
+    goal_amount: u128,
+    bump: u8,
+}
+
+// validation struct
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateUserStats<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    // space = 8 discriminator + 16 expiration + 16 goal_amount + 1 bump
+    #[account(init, payer = user, space = 41, seeds =[b"user_stats", user.key().as_ref()], bump)]
+    pub user_stats: Account<'info, UserStats>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct SetExpiration<'info> {
+    pub user: Signer<'info>,
+    #[account(mut, seeds = [b"user_stats", user.key().as_ref()], bump = user_stats.bump)]
+    pub user_stats: Account<'info, UserStats>,
+}
+
+#[derive(Accounts)]
+pub struct SetGoalAmount<'info> {
+    pub user: Signer<'info>,
+    #[account(mut, seeds = [b"user_stats", user.key().as_ref()], bump = user_stats.bump)]
+    pub user_stats: Account<'info, UserStats>,
+}
--- savus-solana/tests/savus-solana.ts
+++ savus-solana/tests/savus-solana.ts
@@ -1,6 +1,10 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
+import { PublicKey, SystemProgram } from '@solana/web3.js';
 import { SavusSolana } from '../target/types/savus_solana';
+import { expect } from 'chai';
+const BN = require('bn.js');
+
 
 describe('savus-solana', () => {
 
@@ -9,9 +13,23 @@ describe('savus-solana', () => {
 
   const program = anchor.workspace.SavusSolana as Program<SavusSolana>;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  it('Creates user stats', async () => {
+    const [userStatsPDA, _] = await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode("user_stats"),
+        anchor.getProvider().wallet.publicKey.toBuffer()
+      ],
+      program.programId
+    )
+
+    await program.rpc.createUserStats({
+      accounts: {
+        user: anchor.getProvider().wallet.publicKey,
+        userStats: userStatsPDA,
+        systemProgram: SystemProgram.programId
+      }
+    })
+
+    expect((await program.account.userStats.fetch(userStatsPDA)).expiration).to.equal(new BN(0));
   });
 });
